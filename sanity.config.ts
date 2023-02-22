232405cb4cad495f8b5ed90f6dcca0f500033c14
anxhirr <anxhirr@gmail.com>
2023-02-22 13:43:30 +0100

    sanity cli fixes

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,14 +3,11 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
-
 export default defineConfig({
   name: 'default',
   title: 'Anxhi`s Portfolio',
-  projectId,
-  dataset,
+  projectId: 'yciyzigz',
+  dataset: 'production',
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
